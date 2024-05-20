1f334b9e7921e90ac3c8e50d715a83817a8b6dda
God'sgift Uko <godsgiftuko@gmail.com>
2024-05-20 23:53:37 +0100

    Rename variable file to datatypes & add more datatypes.

---
+++ fundamentals/datatypes.dart
@@ -0,0 +1,31 @@
+void main() {
+  String name = 'Gift';
+  print('name: ${name}');
+
+  int phone = 07000000000;
+  print('phone: ${phone}');
+
+  double amount = 15000.00;
+  print('amount: ${amount}');
+
+  const gravity = 9.8;
+  print('gravity: ${gravity}');
+
+  const speedOfLight = '3x10^';
+  print('speedOfLight: ${speedOfLight}');
+
+  const bool isFalse = false;
+  print('isFalse: ${isFalse}');
+
+  const Map<String, int> studentsScore = {'Gift': 10, 'John': 10};
+  print('studentsScore: ${studentsScore}');
+
+  const List<String> studentNames = ['John Doe', 'Henry Adams'];
+  print('studentNames: ${studentNames}');
+
+  const Set<String> bestStudents = {'Gift', 'John'};
+  print('bestStudents: ${bestStudents}');
+
+  const Null theNull = null;
+  print('theNull: ${theNull}');
+}
